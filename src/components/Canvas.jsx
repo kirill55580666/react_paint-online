397e19d16a0fdbbdbcbe16d80998b9c3c43f2374
kirill55580666 <kirill55580666@gmail.com>
2022-05-25 21:14:44 +0500

    init

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,123 @@
+import React, {useEffect, useRef, useState} from 'react';
+import '../styles/canvas.scss'
+import canvasState from "../store/canvasState";
+import {observer} from "mobx-react-lite";
+import {Button, Modal} from "react-bootstrap";
+import {useParams} from "react-router-dom";
+import Brush from "../tools/Brush";
+import Rect from "../tools/Rect";
+import toolState from "../store/toolState";
+import axios from 'axios'
+
+
+const Canvas = observer(() => {
+    const canvasRef = useRef()
+    const usernameRef = useRef()
+    const [modal, setModal] = useState(true)
+    const params = useParams();
+
+    useEffect(() => {
+        canvasState.setCanvas(canvasRef.current)
+        let ctx = canvasRef.current.getContext('2d')
+        axios.get(`http://localhost:5000/image?id=${params.id}`)
+            .then(response => {
+                const img = new Image()
+                img.src = response.data
+                img.onload = () => {
+                    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
+                    ctx.drawImage(img, 0, 0, canvasRef.current.width, canvasRef.current.height)
+                }
+            })
+    }, [])
+
+
+    useEffect(() => {
+        if(canvasState.username) {
+            const socket = new WebSocket('ws://localhost:5000/')
+            canvasState.setSocket(socket)
+            canvasState.setSessionId(params.id)
+            toolState.setTool(new Brush(canvasRef.current, socket, params.id))
+            socket.onopen = () => {
+                console.log(`Соедениние установлено пользователь ${canvasState.username}`);
+                socket.send(JSON.stringify({
+                    method: 'connection',
+                    id: params.id,
+                    username: canvasState.username
+                }))
+            }
+            socket.onmessage = (event) => {
+                let msg = JSON.parse(event.data)
+                switch (msg.method) {
+                    case 'connection':
+                        console.log(`Пользователь присоединился ${canvasState.username}`);
+                        break
+                    case 'draw':
+                        drawHandler(msg)
+                        break
+
+                }
+            }
+        }
+
+    }, [canvasState.username])
+
+    const drawHandler = (msg) => {
+        const figure = msg.figure
+        const ctx = canvasRef.current.getContext('2d')
+        switch (figure.type) {
+            case "brush":
+                Brush.draw(ctx, figure.x, figure.y)
+                break
+            case 'rect':
+                Rect.staticDraw(ctx, figure.x, figure.y, figure.width, figure.height, figure.color)
+                break
+            case "finish":
+                ctx.beginPath();
+                break;
+        }
+    }
+
+    const mouseDownHandler = () => {
+        canvasState.pushToUndo(canvasRef.current.toDataURL())
+    }
+
+    const mouseUpHandler = () => {
+        axios.post(`http://localhost:5000/image?id=${params.id}`, {img: canvasRef.current.toDataURL()})
+            .then(res => console.log(res))
+    }
+
+    const connectHandler = () => {
+        canvasState.setUsername(usernameRef.current.value)
+        setModal(false)
+    }
+
+    return (
+        <div className="canvas">
+            <Modal show={modal} onHide={() => {}} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Введите ваше имя</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <input typ='text' ref={usernameRef}/>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="warning" onClick={() => connectHandler()}>
+                        Войти
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <canvas
+                onMouseDown={() => mouseDownHandler()}
+                onMouseUp={() => mouseUpHandler()}
+                ref={canvasRef}
+                width={600}
+                height={400}
+            >
+
+            </canvas>
+        </div>
+    );
+});
+
+export default Canvas;
---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import '../styles/toolbar.scss'
+import toolState from "../store/toolState";
+import Brush from "../tools/Brush";
+import canvasState from "../store/canvasState";
+import Rect from "../tools/Rect";
+import Circle from "../tools/Circle";
+import Eraser from "../tools/Eraser";
+import Line from "../tools/Line";
+import useDebounce from "../hooks/useDebounce";
+
+const Toolbar = () => {
+
+    const changeColor = useDebounce( (e) => {
+        if(toolState.tool) {
+            toolState.setFillColor(e.target.value);
+            toolState.setStrokeColor(e.target.value);
+        }
+    }, 100)
+
+    const download = () => {
+        const dataUrl = canvasState.canvas.toDataURL()
+        console.log(dataUrl);
+        const a = document.createElement('a')
+        a.href = dataUrl
+        a.download = canvasState.sessionid + ".jpg"
+        document.body.appendChild(a)
+        a.click()
+        document.body.removeChild(a)
+    }
+
+    return (
+        <div className="toolbar">
+            <button
+                className='toolbar__button brush'
+                onClick={() => toolState.setTool(new Brush(canvasState.canvas, canvasState.socket, canvasState.sessionid))}
+            />
+            <button
+                className='toolbar__button rect'
+                onClick={() => toolState.setTool(new Rect(canvasState.canvas, canvasState.socket, canvasState.sessionid))}
+            />
+            <button
+                className='toolbar__button circle'
+                onClick={() => toolState.setTool(new Circle(canvasState.canvas))}
+            />
+            <button
+                className='toolbar__button eraser'
+                onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}
+            />
+            <button
+                className='toolbar__button line'
+                onClick={() => toolState.setTool(new Line(canvasState.canvas))}
+            />
+            <input
+                onChange={e => changeColor(e)}
+                type='color'
+                className='toolbar__button color'
+            />
+            <button
+                onClick={() => canvasState.undo()}
+                className='toolbar__button undo'
+            />
+            <button
+                onClick={() => canvasState.redo()}
+                className='toolbar__button redo'
+            />
+            <button className='toolbar__button save'
+                    onClick={() => download()}
+            />
+        </div>
+    );
+};
+
+export default Toolbar;
